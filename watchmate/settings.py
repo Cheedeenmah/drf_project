1a20fe7af552318356cab540d3b95808de2aaabd
Cheedeenmah <peacechidinmasmooth@gmail.com>
2025-03-20 21:23:48 +0100

    I defined my models

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'watchlist',
-    'restframework',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
